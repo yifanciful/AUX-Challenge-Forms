831bd14100bc16979fc1cb5d5f8be0e687b04f12
yifanciful <yifan.zxu@gmail.com>
2012-10-09 14:27:10 -0400

    Add HTML structure

--- build/index.php
+++ build/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>AUX Form Challenge</title>
 </head>
 
 <body>
@@ -27,7 +27,7 @@
             
             // set the email headers
             $headers = "From: ".$emailFrom."\n";
-            $headers .= "Reply-To:".$emailFrom."\n";	
+            $headers .= "Reply-To:".$emailFrom."\n";    
             
             $success = mail($emailTo, $subject, $body, $headers);
             
@@ -43,19 +43,89 @@
         
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" minlength="2"/>
+            <h1>Sign up for Whoo!</h1>
+            <p>50 projects, 500 images, 10 videos, domain binding, and technical support</p>
+
+            <fieldset>
+            <legend><span>1</span>First, name your portfolio</legend>
+          
+                    <label for="title">Portfolio title</label>
+                    <input type="text" id="title" name="title" minlength="2" />
             
-            <label for="name">E-mail</label>
-            <input type="email" id="email" name="email" minlength="2"/>
-            
-            <button type="submit" name="send">Send!</button>
-            
+                    <label for="address">Portfolio address</label>
+                    <input type="url" id="address" name="address" minlength="2" />
+          
+            </fieldset>
+
+            <fieldset>
+            <legend><span>2</span>Now, enter your account details</legend>  
+          
+           
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" required />
+         
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+                    <aside>NOTE: We'll never share your email, promise.</aside>
+    
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required />
+                    <input type="checkbox" id="show_pass" name="show_pass" value="1">
+                    Show Password    
+         
+            </fieldset>
+
+            <fieldset>
+                <legend><span>3</span>Finally, enter your payment information <a href="#">Use PayPal</a></legend>
+
+                    <label for="number">Card number</label>
+                    <input type="text" id="number" name="cardnumber" required />
+           
+      
+                    <label for="security">Security code</label>
+                    <input type="text" id="security" name="security" required />
+         
+          
+                    <label for="expiration">Expiration date:</label>
+                    <select id="expiration_month" name="expiration_month">
+                        <option value="">Month</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                    </select>
+                    <select id="expiration_year" name="expiration_year">
+                        <option value="">Year</option>
+                        <option value="2012">2012</option>
+                        <option value="2013">2013</option>
+                        <option value="2014">2014</option>
+                        <option value="2015">2015</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                    </select>
+         
+            </fieldset>
+
+    
+            <button type ="submit" name="send">Create your portfolio</button>
+        
+
         </form>
     
     <?php
         }
     ?>
+
+
 
 </body>
 </html>
